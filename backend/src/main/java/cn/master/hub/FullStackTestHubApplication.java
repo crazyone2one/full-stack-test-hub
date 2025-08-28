2d4d1c8e24c478599cfcc9b7d2b3c54509543ed3
lijinglin <the2ndindec@gmail.com>
2025-08-28 14:10:41 +0800

    init repository

---
+++ backend/src/main/java/cn/master/hub/FullStackTestHubApplication.java
@@ -0,0 +1,13 @@
+package cn.master.hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackTestHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullStackTestHubApplication.class, args);
+    }
+
+}
